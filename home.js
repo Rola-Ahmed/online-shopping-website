68dbf74aded72be7b1cd4a67070b6695a9ad0817
Rola-Ahmed <75480678+Rola-Ahmed@users.noreply.github.com>
2022-10-19 06:27:42 +0200

    .

--- home.js
+++
@@ -1,9 +0,0 @@
-// function clickCategoriy(){
- 
-//     <style>
-// .TopNav2 .TopNav2-Category .Category ul{ /*keda 3mla hover 3la el list 7ata law bnzl t7t*/
-//   /*.Topnav-DropDown-List .DD-Title:hover + ul*/
-//   display: block; /*ي عشان يخلي الكلام الي جواه تختفي وبعد كده لما يعمل hover تبقا display block عشان تظهر بس لما الموس يجي عليه*/
-// }
-//     </style>
-// }
